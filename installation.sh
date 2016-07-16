27a36a70ea08343d41f4843975b221e9b1c3210e
Ubuntu <ubuntu@ip-10-140-157-172.ec2.internal>
2016-07-16 12:24:53 +0000

    updated pathways

--- installation.sh
+++ installation.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 sudo apt-get update
 sudo apt-get install scons libboost-all-dev g++ libz-dev libncurses5-dev libbam-dev make
-cd metabat_clone/src
+cd ~/metabat_clone/src
 wget http://lh3lh3.users.sourceforge.net/download/kseq.tar.bz2
 tar -xvjf kseq.tar.bz2
 cd ..
